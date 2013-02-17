06b901bb90b988d873f7136e3f463f3319d8dad0
Soeed <a727891@gmail.com>
2013-02-17 17:28:37 -0600

    Began seam tile creation

--- Client/js/Renderer.js
+++ Client/js/Renderer.js
@@ -9,13 +9,29 @@ define(['SimplexNoise', 'TileData'], function (Simplex, TileData) {
             this.TileData = new TileData();
             this.LoadCanvas('TestCanvas');
             this.Simplex = new Simplex(this.Canvas, this.Context);
-            this.Simplex.config.color = true;
-            this.Simplex.config.colorlevels = this.TileData.WorldTiles;
-            this.Simplex.GenImage(null);
+            this.Simplex.config.color = false;
+//            this.Simplex.config.colorlevels = this.TileData.WorldTiles;
+//            this.Simplex.GenImage(null);
 //            this.Simplex.InsertControls();
 //            var Tiles = this.Simplex.TilesData;
             this.Simplex.config.color = true;
-            this.GenTileMap();
+//            this.GenTileMap();
+
+            this.Context.save();
+            this.Context.fillStyle = 'white';
+//            this.Context.clearRect(0,0, this.Canvas.width,this.Canvas.height);
+            this.Context.arc(0, 0, this.Canvas.width / 2, 0, Math.PI, 0);
+            this.Context.fill();
+            this.Context.clear();
+            this.Context.arc(0, this.Canvas.height, this.Canvas.width / 2, 3 * Math.PI, .5 * Math.PI, 0);
+            this.Context.fill();
+            this.Context.clear();
+            this.Context.arc(this.Canvas.width, 0, this.Canvas.width / 2, 0, Math.PI, 1);
+            this.Context.fill();
+            this.Context.clear();
+            this.Context.arc(this.Canvas.width, this.Canvas.height, this.Canvas.width / 2, Math.PI, 2 * Math.PI, 1);
+            this.Context.fill();
+            this.Context.clear();
 
         },
         LoadCanvas:function (canvas) {
@@ -40,19 +56,25 @@ define(['SimplexNoise', 'TileData'], function (Simplex, TileData) {
                 tile_x,
                 tile_y;
 
-            console.log("MaxX:" + max_x, "MaxY:" + max_y, 'tW:' + t_w, 'tH:' + t_h);
-            for (var i = 0; i < this.TileData.Tiles.length; i++) {
-                console.log("Generate tile called: " + this.TileData.Tiles[i].name);
-                this.Simplex.config.colorlevels = this.TileData.Tiles[i].colors;
-                this.Simplex.GenImage(this.TileData.Tiles[i].seed);
-                tile_y = t_h * Math.floor(i / max_y);
-                tile_x = t_w * (i % max_y);
-                console.log(i, "X:", tile_x, "Y:", tile_y);
-//            context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
-                this.TileMapContext.drawImage(this.Canvas
-                    , 0, 0, t_w, t_h,
-                    tile_x, tile_y, t_w, t_h);
-            }
+//            console.log("MaxX:" + max_x, "MaxY:" + max_y, 'tW:' + t_w, 'tH:' + t_h);
+//            for (var i = 0; i < this.TileData.Tiles.length; i++) {
+//                console.log("Generate tile called: " + this.TileData.Tiles[i].name);
+//                this.Simplex.config.colorlevels = this.TileData.Tiles[i].colors;
+//                this.Simplex.GenImage(this.TileData.Tiles[i].seed);
+//                tile_y = t_h * Math.floor(i / max_y);
+//                tile_x = t_w * (i % max_y);
+//                console.log(i, "X:", tile_x, "Y:", tile_y);
+////            context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
+//                this.TileMapContext.drawImage(this.Canvas
+//                    , 0, 0, t_w, t_h,
+//                    tile_x, tile_y, t_w, t_h);
+//            }
+
+            this.Context.save();
+            this.Context.clearRect(0, 0, this.Canvas.width, this.Canvas.height);
+            this.Context.arc(0, 0, t_h / 2, 0, Math.PI, 0);
+            this.Context.fill();
+
 
         }
 
